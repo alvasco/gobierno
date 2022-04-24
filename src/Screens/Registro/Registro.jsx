9b32a3668cb0ac76bb720092dfc9c8917ecc6e2d
Luis alberto Sinisterra muñoz <brutalblack91@gmail.com>
2022-04-23 21:41:04 -0500

    cambios warnings

--- src/Screens/Registro/Registro.jsx
+++ src/Screens/Registro/Registro.jsx
@@ -25,8 +25,8 @@ const Registro = () => {
               width="100%"
               height="700"
               frameBorder="0"
-              marginheight="0"
-              marginwidth="0"
+              marginHeight="0"
+              marginWidth="0"
               title="Registro"
             ></iframe>
           </Col>
